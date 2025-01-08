2151cd4cb9308adb3ac76a28669bdd29b9753c21
DoanDucBao <psawn0972055909@yahoo.com.vn>
2025-01-08 17:14:46 +0700

    update

--- src/main/java/com/example/demo/rest/FunRestController.java
+++ src/main/java/com/example/demo/rest/FunRestController.java
@@ -1,17 +1,38 @@
 package com.example.demo.rest;
 
+import com.example.demo.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    private final Coach myCoach;
+
+    @Autowired
+    public FunRestController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
     }
 
-    @GetMapping("/workout")
+    @GetMapping("/daily-workout")
     public String getDailyWorkout() {
-        return "Daily workout";
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/team-info")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team: " + teamName;
     }
 }
